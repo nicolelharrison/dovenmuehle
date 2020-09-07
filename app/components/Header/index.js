59e6aaf42af7a93dd68e3a5c9c7680591ff4b773
Nicole Harrison <nicki.harrison@gmail.com>
2020-09-07 10:19:05 -0700

    update header button names and remove footer

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
 
 import A from './A';
 import Img from './Img';
 import NavBar from './NavBar';
 import HeaderLink from './HeaderLink';
 import Banner from './banner.jpg';
-import messages from './messages';
 
 function Header() {
   return (
@@ -15,12 +13,8 @@ function Header() {
         <Img src={Banner} alt="react-boilerplate - Logo" />
       </A>
       <NavBar>
-        <HeaderLink to="/">
-          <FormattedMessage {...messages.home} />
-        </HeaderLink>
-        <HeaderLink to="/features">
-          <FormattedMessage {...messages.features} />
-        </HeaderLink>
+        <HeaderLink to="/">Enter String</HeaderLink>
+        <HeaderLink to="/features">View Strings</HeaderLink>
       </NavBar>
     </div>
   );
--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -1,11 +1,3 @@
-/**
- *
- * App
- *
- * This component is the skeleton around the actual pages, and should only
- * contain code that should be seen on all pages. (e.g. navigation bar)
- */
-
 import React from 'react';
 import { Helmet } from 'react-helmet';
 import styled from 'styled-components';
@@ -15,7 +7,6 @@ import HomePage from 'containers/HomePage/Loadable';
 import FeaturePage from 'containers/FeaturePage/Loadable';
 import NotFoundPage from 'containers/NotFoundPage/Loadable';
 import Header from 'components/Header';
-import Footer from 'components/Footer';
 
 import GlobalStyle from '../../global-styles';
 
@@ -40,10 +31,9 @@ export default function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/features" component={FeaturePage} />
+        <Route path="/strings" component={FeaturePage} />
         <Route path="" component={NotFoundPage} />
       </Switch>
-      <Footer />
       <GlobalStyle />
     </AppWrapper>
   );

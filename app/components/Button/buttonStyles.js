49dff953152e6ac5b4c7b5c09e67e6f4e8b36086
Nicole Harrison <nicki.harrison@gmail.com>
2020-09-07 17:20:56 -0700

    add conditional styles to button component

--- app/components/Button/buttonStyles.js
+++ app/components/Button/buttonStyles.js
@@ -9,13 +9,15 @@ const buttonStyles = css`
   -webkit-font-smoothing: antialiased;
   -webkit-touch-callout: none;
   user-select: none;
-  cursor: pointer;
+  cursor: ${props => (props.disabled ? 'not-allowed' : 'pointer')};
   outline: 0;
   font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
   font-weight: bold;
   font-size: 16px;
   border: 2px solid #41addd;
-  color: #41addd;
+  color: ${props => (props.primary ? '#fff' : '#41addd')};
+  background-color: ${props => (props.primary ? '#41addd' : '#fff')};
+  margin-top: ${props => (props.primary ? '2em' : '0em')};
 
   &:active {
     background: #41addd;
